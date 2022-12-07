440b4f3d0b11619ff1b311e2e9fc8b394252d6cd
Antonio <antonio-emilio_6@hotmail.com>
2022-12-07 20:29:47 -0300

    first-commit

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express, { request, response } from 'express';
+import routes from './routes';
+const app = express();
+
+app.use(routes)
+
+
+app.listen(3333,()=>{
+    console.log('Server iniciado na porta 3333')
+});
